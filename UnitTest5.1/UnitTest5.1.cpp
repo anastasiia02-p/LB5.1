196a27bc4f7a10ed0416a29db2006ff57c8edeb6
Nastya Prunko <nastya.prunko2007@gmail.com>
2024-09-30 23:53:49 +0300

    second test

---
+++ UnitTest5.1/UnitTest5.1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LB5.1/main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest51
+{
+	TEST_CLASS(UnitTest51)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double result = f(3, 1, 3);
+			Assert::AreEqual(result, 11.6 , 0.1);
+		}
+	};
+}
